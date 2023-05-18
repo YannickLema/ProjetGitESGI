7b8644a94e0b5c8c91fa7b95779fd0cfc01ace08
YannickLema <70702750+YannickLema@users.noreply.github.com>
2023-05-18 17:28:21 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// Empécher le déclenchement des transitions au chargement de la page
+document.getElementById('site-body').classList.remove('no-transition');
+
+// Ajout d'un écouteur d'événement sur le menu-button
+// document.getElementById('menu-button').addEventListener('click', function(){
+// 	document.getElementById('site-nav').classList.toggle('visible');
+// })
+
+// const darkModeToggle = document.querySelector('#dark-mode-toggle');
+
+//   darkModeToggle.addEventListener('click', function() {
+//     document.body.classList.toggle('dark-mode');
+//   });
+
+// // Redirection vers Facebook
+// document.getElementById('facebookLink').addEventListener('click', function() {
+//   window.location.href = 'https://www.facebook.com';
+// });
+
+// // Redirection vers Twitter
+// document.getElementById('twitterLink').addEventListener('click', function() {
+//   window.location.href = 'https://www.twitter.com';
+// });
+
+// // Redirection vers Instagram
+// document.getElementById('instagramLink').addEventListener('click', function() {
+//   window.location.href = 'https://www.instagram.com';
+// });
+
+// var promoVideo = document.getElementById('promoVideo');
+// var playButton = document.getElementById('playButton');
+// var videoOverlay = document.getElementById('videoOverlay');
+
+// playButton.addEventListener('click', function() {
+//   if (promoVideo.paused) {
+//     promoVideo.play();
+//     videoOverlay.style.display = 'none';
+//   } else {
+//     promoVideo.pause();
+//     videoOverlay.style.display = 'flex';
+//   }
+// });
